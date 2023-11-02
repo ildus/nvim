84d634f9232cb8ad1c2e4b5fdbbba1ce110ff0eb
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2023-11-02 22:58:35 +0100

    fix telescope (upgrade)

--- lua/init_lazy.lua
+++ lua/init_lazy.lua
@@ -82,7 +82,7 @@ require("lazy").setup({
 
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
+    tag = '0.1.4',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
