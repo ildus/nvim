53f965e615ee2396524fa5b6ba6834a59bf38930
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2024-10-09 14:53:13 +0200

    remove gutentags

--- lua/init_lazy.lua
+++ lua/init_lazy.lua
@@ -55,19 +55,6 @@ require("lazy").setup({
       }
     },
   },
-  {
-    "ludovicchabant/vim-gutentags",
-    dependencies = {
-      "dhananjaylatkar/cscope_maps.nvim",
-    },
-      init = function()
-        vim.g.gutentags_modules = {"cscope_maps"} -- This is required. Other config is optional
-        vim.g.gutentags_cscope_build_inverted_index_maps = 1
-        vim.g.gutentags_cache_dir = vim.fn.expand("~/.cache/gutentags")
-        vim.g.gutentags_file_list_command = "fd -Hu -e qsh -e sc -e qsc -e c -e cc -e cpp -e h -e hh -e hpp"
-        -- vim.g.gutentags_trace = 1
-      end,
-  },
   'alok/notational-fzf-vim',
   'justinmk/vim-syntax-extra',
   {
