f381fda1d499fb7f0b3667a89bd456289395cc6b
Ildus <ildus.kurbangaliev@actian.com>
2025-02-02 11:20:05 +0100

    Try to always use OSC52, supported by kitty

--- lua/init_lazy.lua
+++ lua/init_lazy.lua
@@ -11,6 +11,18 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
 require("lazy").setup({
   'ressu/vim-xdg-cache',
   'ntpeters/vim-better-whitespace',
